e3eeb6c08336e0e59e6e74bb5a10b2e498b2e36b
Molly <m73382846@users.noreply.github.com>
2020-08-27 22:26:32 -0600

    complete resistor color duo

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,14 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
+
+
+export const decodedValue = (resistorValues) => {
+  const firstColor = COLORS.indexOf(resistorValues[0]).toString();
+  const secondColor = COLORS.indexOf(resistorValues[1]).toString();
+
+  return  parseInt(firstColor + secondColor); 
+
+
 };
