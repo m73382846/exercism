bb9e4118fcdf6a64f1a896b36ad1c748cfd5688c
Molly <m73382846@users.noreply.github.com>
2020-08-27 22:59:49 -0600

    updated final update

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -4,18 +4,14 @@
 //
 
 export const isLeap = (year) => {  
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      
-      if (year % 400 === 0) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return true;
-    }
-  } else {
+  if (year % 4 !== 0) {
+    return false; 
+  }
+  if (year % 100 !== 0) {
+    return true;
+  }
+  if (year % 400 !== 0) {
     return false;
-  };
+  }
+  return true;
 };
