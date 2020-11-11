03795ee0ca5b7854b711ea9dcdf3f797db78f811
Molly <m73382846@users.noreply.github.com>
2020-11-10 22:02:56 -0700

    all tests pass and it's prettier

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -4,36 +4,39 @@
 //
 
 export const age = (planet, time) => {
+  const rounded = (number) => {
+    return Number((number).toFixed(2)); 
+   }
   const earthYears = (time/60/60/24/365.25)
-    if (planet === 'earth') {
-      return Number((earthYears).toFixed(2));
-    } 
-    if (planet === 'mercury') {
-      const mercuryYears = (earthYears/0.2408467);
-        return Number((mercuryYears).toFixed(2));
-    } 
-    if (planet === 'venus') {
-      const venusYears = (earthYears/0.61519726);
-        return Number((venusYears).toFixed(2));
-    } 
-    if (planet === 'mars') {
-      const marsYears = (earthYears/1.8808158);
-        return Number((marsYears).toFixed(2));
-    } 
-    if (planet === 'jupiter') {
-      const jupiterYears = (earthYears/11.862615);
-        return Number((jupiterYears).toFixed(2));
-    } 
-    if (planet === 'saturn') {
-      const saturnYears = (earthYears/29.447498);
-        return Number((saturnYears).toFixed(2));
-    } 
-    if (planet === 'uranus') {
-      const uranusYears = (earthYears/84.016846);
-        return Number((uranusYears).toFixed(2));
-    } 
-    if (planet === 'neptune') {
-      const neptuneYears = (earthYears/164.79132);
-        return Number((neptuneYears).toFixed(2));
-    }    
+  if (planet === 'earth') {
+    return rounded(earthYears);
+  } 
+  if (planet === 'mercury') {
+    const mercuryYears = (earthYears/0.2408467);
+    return rounded(mercuryYears);
+  } 
+  if (planet === 'venus') {
+    const venusYears = (earthYears/0.61519726);
+    return rounded(venusYears);
+  } 
+  if (planet === 'mars') {
+    const marsYears = (earthYears/1.8808158);
+    return rounded(marsYears);
+  } 
+  if (planet === 'jupiter') {
+    const jupiterYears = (earthYears/11.862615);
+    return rounded(jupiterYears);
+  } 
+  if (planet === 'saturn') {
+    const saturnYears = (earthYears/29.447498);
+    return rounded(saturnYears);
+  } 
+  if (planet === 'uranus') {
+    const uranusYears = (earthYears/84.016846);
+    return rounded(uranusYears);
+  } 
+  if (planet === 'neptune') {
+    const neptuneYears = (earthYears/164.79132);
+    return rounded(neptuneYears);
+  }
 };
