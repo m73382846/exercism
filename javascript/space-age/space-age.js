bcb50591cf2e7aec7aa4a77e65175b009f67564c
Molly <m73382846@users.noreply.github.com>
2020-11-10 22:17:29 -0700

    all tests pass and it's the prettiest

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -8,35 +8,15 @@ export const age = (planet, time) => {
     return Number((number).toFixed(2)); 
    }
   const earthYears = (time/60/60/24/365.25)
-  if (planet === 'earth') {
-    return rounded(earthYears);
-  } 
-  if (planet === 'mercury') {
-    const mercuryYears = (earthYears/0.2408467);
-    return rounded(mercuryYears);
-  } 
-  if (planet === 'venus') {
-    const venusYears = (earthYears/0.61519726);
-    return rounded(venusYears);
-  } 
-  if (planet === 'mars') {
-    const marsYears = (earthYears/1.8808158);
-    return rounded(marsYears);
-  } 
-  if (planet === 'jupiter') {
-    const jupiterYears = (earthYears/11.862615);
-    return rounded(jupiterYears);
-  } 
-  if (planet === 'saturn') {
-    const saturnYears = (earthYears/29.447498);
-    return rounded(saturnYears);
-  } 
-  if (planet === 'uranus') {
-    const uranusYears = (earthYears/84.016846);
-    return rounded(uranusYears);
-  } 
-  if (planet === 'neptune') {
-    const neptuneYears = (earthYears/164.79132);
-    return rounded(neptuneYears);
+  const ratioYears = {
+    earth: 1, 
+    mercury: 0.2408467,
+    venus: 0.61519726, 
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
   }
+  return rounded(earthYears/ratioYears[planet]);
 };
