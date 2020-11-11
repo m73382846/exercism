db8eb43b2f82db3c40a42c0b0c6c4c70324604b1
Molly <m73382846@users.noreply.github.com>
2020-11-10 21:46:52 -0700

    all test pass but less math

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -4,29 +4,36 @@
 //
 
 export const age = (planet, time) => {
-  let years = (time/60/60/24/365.25)
+  const earthYears = (time/60/60/24/365.25)
     if (planet === 'earth') {
-      return Number((years).toFixed(2));
-    } if (planet === 'mercury') {
-      let years = (time/60/60/24/365.25/0.2408467);
-        return Number((years).toFixed(2));
-    } if (planet === 'venus') {
-      let years = (time/60/60/24/365.25/0.61519726);
-        return Number((years).toFixed(2));
-    } if (planet === 'mars') {
-      let years = (time/60/60/24/365.25/1.8808158);
-        return Number((years).toFixed(2));
-    } if (planet === 'jupiter') {
-      let years = (time/60/60/24/365.25/11.862615);
-        return Number((years).toFixed(2));
-    } if (planet === 'saturn') {
-      let years = (time/60/60/24/365.25/29.447498);
-        return Number((years).toFixed(2));
-    } if (planet === 'uranus') {
-      let years = (time/60/60/24/365.25/84.016846);
-        return Number((years).toFixed(2));
-    } if (planet === 'neptune') {
-      let years = (time/60/60/24/365.25/164.79132);
-        return Number((years).toFixed(2));
+      return Number((earthYears).toFixed(2));
+    } 
+    if (planet === 'mercury') {
+      const mercuryYears = (earthYears/0.2408467);
+        return Number((mercuryYears).toFixed(2));
+    } 
+    if (planet === 'venus') {
+      const venusYears = (earthYears/0.61519726);
+        return Number((venusYears).toFixed(2));
+    } 
+    if (planet === 'mars') {
+      const marsYears = (earthYears/1.8808158);
+        return Number((marsYears).toFixed(2));
+    } 
+    if (planet === 'jupiter') {
+      const jupiterYears = (earthYears/11.862615);
+        return Number((jupiterYears).toFixed(2));
+    } 
+    if (planet === 'saturn') {
+      const saturnYears = (earthYears/29.447498);
+        return Number((saturnYears).toFixed(2));
+    } 
+    if (planet === 'uranus') {
+      const uranusYears = (earthYears/84.016846);
+        return Number((uranusYears).toFixed(2));
+    } 
+    if (planet === 'neptune') {
+      const neptuneYears = (earthYears/164.79132);
+        return Number((neptuneYears).toFixed(2));
     }    
 };
